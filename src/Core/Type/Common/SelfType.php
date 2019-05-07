cefbf902f4d4c1c2f2adbf8f101e4ddd7994767c
Gytis Skema <gytis.skema@gmail.com>
2019-05-07 17:06:26 +0300

    Remove dead code from SelfType

--- src/Core/Type/Common/SelfType.php
+++ src/Core/Type/Common/SelfType.php
@@ -6,19 +6,6 @@ use Gskema\TypeSniff\Core\Type\TypeInterface;
 
 class SelfType implements TypeInterface
 {
-    /** @var string */
-    protected $fqcn;
-
-    public function __construct()
-    {
-        $this->fqcn = '';
-    }
-
-    public function getFqcn(): string
-    {
-        return $this->fqcn;
-    }
-
     /**
      * @inheritDoc
      */

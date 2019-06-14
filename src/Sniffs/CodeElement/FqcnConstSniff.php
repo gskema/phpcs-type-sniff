9ad7b6738fd124fac0c4892c27f32f6bf81a5dc9
Gytis Skema <gytis.skema@gmail.com>
2019-06-14 17:08:58 +0300

    Use constant $valueType to add array PHPDoc warning

--- src/Sniffs/CodeElement/FqcnConstSniff.php
+++ src/Sniffs/CodeElement/FqcnConstSniff.php
@@ -8,7 +8,6 @@ use Gskema\TypeSniff\Core\CodeElement\Element\ClassConstElement;
 use Gskema\TypeSniff\Core\CodeElement\Element\CodeElementInterface;
 use Gskema\TypeSniff\Core\CodeElement\Element\InterfaceConstElement;
 use Gskema\TypeSniff\Core\DocBlock\Tag\VarTag;
-use Gskema\TypeSniff\Core\DocBlock\UndefinedDocBlock;
 use Gskema\TypeSniff\Core\Type\Common\ArrayType;
 
 class FqcnConstSniff implements CodeElementSniffInterface
@@ -44,19 +43,21 @@ class FqcnConstSniff implements CodeElementSniffInterface
 
         /** @var VarTag|null $varTag */
         $varTag = $docBlock->getTagsByName('var')[0] ?? null;
-        $varType = $varTag ? $varTag->getType() : null;
+        $docType = $varTag ? $varTag->getType() : null;
 
-        // PHPDoc for const are usually not necessary.
-        if ($docBlock instanceof UndefinedDocBlock) {
-            return;
-        }
+        $subject = $const->getConstName().' constant';
 
-        if ($varType instanceof ArrayType) {
-            $subject = $const->getConstName().' constant';
+        if ($docType instanceof ArrayType) {
             $file->addWarningOnLine(
                 'Replace array type with typed array type in PHPDoc for '.$subject.'. Use mixed[] for generic arrays.',
                 $const->getLine(),
-                'FqcnPropSniff'
+                'FqcnConstSniff'
+            );
+        } elseif (is_a($const->getValueType(), ArrayType::class)) {
+            $file->addWarningOnLine(
+                'Add PHPDoc with typed array type hint for '.$subject.'. Use mixed[] for generic arrays.',
+                $const->getLine(),
+                'FqcnConstSniff'
             );
         }
 
@@ -64,7 +65,7 @@ class FqcnConstSniff implements CodeElementSniffInterface
             $file->addWarningOnLine(
                 'Remove property name $'.$varTag->getParamName().' from @var tag',
                 $const->getLine(),
-                'FqcnPropSniff'
+                'FqcnConstSniff'
             );
         }
     }
